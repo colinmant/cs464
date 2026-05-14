e1d95336f51bb95b189656f56ec6fc432d10c0d4
Dan White <whiteda3@oregonsate.edu>
2026-05-14 16:12:26 -0700

    resolved build issues

--- src/app/api/data/DELETE.ts
+++ src/app/api/data/DELETE.ts
@@ -20,10 +20,10 @@ export async function DELETE(
     }
 
     if (!data || data.length === 0) {
-      return { status: 404, message: `Dataset with slug ${slug} not found.`}
+      return Response.json({ message: `Dataset with slug ${slug} not found.` }, { status: 404 })
     }
 
-    return Response.json({ message: "Successfully deleted", deletedItem: data[0]}, { status: 200 })
+    return Response.json({ message: "Successfully deleted", deletedItem: data[0] }, { status: 200 })
   }
   return Response.json({ message: "No slug provided" }, { status: 400 })
 }
--- src/app/api/data/PUT.ts
+++ src/app/api/data/PUT.ts
@@ -1,15 +1,19 @@
 import * as z from "zod";
 import { getSupabaseClient } from "@/lib/supabase";
 import { NextRequest } from "next/server";
+import { DatasetItem, DatasetDatabaseItem } from '@/types/data'
 
 export async function PUT(
   request: NextRequest) {
   try {
     const { searchParams } = new URL(request.url)
     const slug = searchParams.get('slug')
+    if (slug == null){
+      return Response.json({ error: "Missing required query parameter: slug" }, { status: 400 })
+    }
     const supabase = getSupabaseClient();
     const body = await request.json();
-    const orderSet = new Set(body.items.map(i => i.order));
+    const orderSet = new Set(body.items.map((i: DatasetItem) => i.order));
     if (orderSet.size !== body.items.length) {
       return Response.json({ error: "Item order values must be unique" }, { status: 400 });
     }
@@ -25,11 +29,14 @@ export async function PUT(
       .select('id')
       .maybeSingle();
     if (updateError) return Response.json({ error: updateError.message }, { status: 500 });
+    if (dataset == null){
+      return Response.json({ error: "Dataset Not Found" }, { status: 400 });
+    }
     const datasetId = dataset.id;
 
     await supabase.from('dataset_items').delete().eq('dataset_id', datasetId);
 
-    const itemsToInsert = body.items.map(item => ({
+    const itemsToInsert = body.items.map((item: DatasetItem) => ({
       dataset_id: datasetId,
       item_name: item.name,
       item_order: item.order,
